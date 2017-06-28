407c1b1e015864acfcb02aa7420d92f2d9016fb6
Em Utdorm <sem2@zamanuniversity.edu.kh>
2017-06-28 11:48:33 +0700

    add some featured

--- circle.c
+++ circle.c
@@ -1,30 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
+#define M_PI acos(-1.0)
+
+struct point {
+    double x;
+    double y;
+};
 
 struct circle {
     double x;
     double y;
     double radius;
+    
 };
 
-struct arc {
-    double a;
-    double b;
+
+struct arc_point {
+    double start;
+    double end;
 };
 
-double arcLength(struct circle c, struct arc arc1){
+struct arc_angle {
+    double angle;
+};
 
-    double result = 2 * 3.14 * c.radius * ( (arc1.b - arc1.a ) / 360);
+double arcLength(struct circle c, struct arc_point arc1) {
+    double result = 2 * M_PI * c.radius * ( (arc1.end - arc1.start) / 360 );
+    return result;
+}
+
+double arcLength1(struct circle c, struct arc_angle arc){
+    
+    double result = 2 * M_PI * c.radius * ( arc.angle / 360);
 
     return result;
 }
 
 int main(void) {
 
-    struct circle A = {0, 0, 10};
-    struct arc a = {0, 90};
-
-    printf("%f", arcLength(A, a));
+    struct circle C = {0, 0, 10};
+    struct arc_point A = {0, 360};
+    struct arc_angle A1 = {360};
+    
+    printf("%f\n", arcLength(C, A));
+    printf("%f", arcLength1(C, A1));
 
 
     return 0;
